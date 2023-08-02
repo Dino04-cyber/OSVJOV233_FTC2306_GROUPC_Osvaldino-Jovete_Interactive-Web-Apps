19f7a37d023e8429ad1dffeb93292bd3c2a2bfd4
Dino04-cyber <djovety@gmail.com>
2023-08-02 13:37:52 +0200

    completed challenege 2

---
+++ OSVJOV233_FTC2306_GROUPC_OsvaldinoJovete_IWA3/Challenge 2/alex.js
@@ -0,0 +1,6 @@
+const firstname = "Alex"
+const surname = "Naidoo"
+export const role3 = "Head of Marketing"
+
+const display= firstname + " " + surname + " (" + role3 + ")"
+document.querySelector('#alex').innerText = display
---
+++ OSVJOV233_FTC2306_GROUPC_OsvaldinoJovete_IWA3/Challenge 2/script.js
@@ -0,0 +1,7 @@
+// console.log('Roles:', 'nwabisa.role,', 'johannes.role,', 'alex.role');
+// main.js
+import { role1 } from './nwabisa.js';
+import { role2 } from './johannes.js';
+import { role3 } from './alex.js';
+
+console.log('Roles:', role1 + ',', role2 + ',', role3);
