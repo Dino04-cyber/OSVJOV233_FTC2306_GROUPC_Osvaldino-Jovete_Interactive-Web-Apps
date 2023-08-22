b2e13af599c21e108ea9c72e8d71689c3c964fea
Dino04-cyber <djovety@gmail.com>
2023-08-22 15:47:31 +0200

    IWA14 Challenge 1

---
+++ OSVJOV233_FTC2306_GROUPC_OsvaldinoJovete_IWA14/challenge1.js
@@ -0,0 +1,17 @@
+//added 'const' to define the variables 
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+//added 'Parameter' to the log twice function so it can be defined
+const logTwice = (Parameter) => {
+  console.log(Parameter)
+  console.log(Parameter)
+}
+
+//gave the function a new name since hobby was already defined at the top
+function greetHobby () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+greetHobby()
