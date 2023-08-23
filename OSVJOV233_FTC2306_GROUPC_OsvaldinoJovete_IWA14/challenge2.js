6e7e0fee1aeeba1a3fed1c03c092078ed5ec0829
Dino04-cyber <djovety@gmail.com>
2023-08-23 09:23:55 +0200

    IWA 14

--- OSVJOV233_FTC2306_GROUPC_OsvaldinoJovete_IWA14/challenge2.js
+++ OSVJOV233_FTC2306_GROUPC_OsvaldinoJovete_IWA14/challenge2.js
@@ -1,45 +1,40 @@
+
+
 // script.js
 
-const add = (a, b) => {
-    return a + b;
-  }
-  
-  const multiply = (a, b) => {
-    return a * b;  // Corrected the multiplication operation
-  }
-  
-  const internal = function() {
-    this.added = add(this.a, this.b);  // Changed to use 'this.added'
-    this.multiplied = multiply(this.a, this.b);  // Added 'this.multiplied'
+const add = (a, b) => { return a + b; }
+const multiply = (a, b) => { return a * b; } // Fixed the multiplication operation
+
+function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    const multiplied = this.multiply(this.internal.a, this.internal.b);
+    console.log(multiplied * added);
     return this;
-  }
-  
+}
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
+    internal: {
+        a: 2,
+        b: 4,
+        c: 8,
+    },
+    add,
+    multiply,
+    calculate: internal,
+};
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
+    internal: {
+        a: 2,
+        b: 2,
+        c: 3,
+    },
+    add,
+    multiply,
+    calculate: internal,
+};
 
 example1.calculate();
 example2.calculate();
-
-
